83ff17ce0a16c735f37771f1d024583a6090a3ec
Ritish <ritish.dhiman@qapita.com>
2024-07-01 11:10:15 +0530

    Logging added

--- DemoProject/Features/Query/GetAllUserQueryHandler.cs
+++ DemoProject/Features/Query/GetAllUserQueryHandler.cs
@@ -7,15 +7,30 @@ namespace DemoProject.Features.Query;
 public class GetAllUserQueryHandler: IRequestHandler<GetAllUserQuery,List<User>> 
 {
     private readonly IUserRepository _userRepository;
+    private readonly Serilog.ILogger _logger;
     
-    public GetAllUserQueryHandler(IUserRepository userRepository)
+    public GetAllUserQueryHandler(IUserRepository userRepository, Serilog.ILogger logger)
     {
         _userRepository = userRepository;
+        _logger = logger;
 
     }
     
     public async Task<List<User>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
     {
-        return await _userRepository.GetUserList(request, CancellationToken.None);
+        try
+        {
+            var users= await _userRepository.GetUserList(request, CancellationToken.None);
+            if (users != null) 
+                return users;
+            _logger.Information("Empty list of users");
+           
+        }
+        catch (Exception ex)
+        {
+            _logger.Information("Error retrieving user details", ex.Message.ToString());
+        }
+
+        return null!;
     }
 }
--- DemoProject/Features/Query/GetUserQueryHandler.cs
+++ DemoProject/Features/Query/GetUserQueryHandler.cs
@@ -7,14 +7,28 @@ namespace DemoProject.Features.Query;
 public class GetUserQueryHandler : IRequestHandler<GetUserQuery,User>
 {
     private readonly IUserRepository _userRepository;
+    private readonly Serilog.ILogger _logger;
 
-    public GetUserQueryHandler(IUserRepository userRepository)
+    public GetUserQueryHandler(IUserRepository userRepository,Serilog.ILogger logger)
     {
         _userRepository = userRepository;
+        _logger = logger;
     }
+
     public async Task<User> Handle(GetUserQuery request, CancellationToken cancellationToken)
     {
-        var user = await _userRepository.GetUser(request.Name, CancellationToken.None);
-        return user;
+        try
+        {
+            var user = await _userRepository.GetUser(request.Name, CancellationToken.None);
+            if (user != null) return user;
+            _logger.Information("User '{UserName}' not found", request.Name);
+        }
+        catch (Exception e)
+        {
+            _logger.Information("Error retrieving user {UserName}", request.Name);
+            throw;
+        }
+
+        return null!;
     }
 }
